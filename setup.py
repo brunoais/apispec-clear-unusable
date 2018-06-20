94b197e9bad3346086c68f30827641e2c51b87e8
brunoais <brunoaiss@gmail.com>
2018-06-20 11:29:52 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='apispec-clear-unusable',
+      version='1.0.0',
+      license='MIT',
+      description='Plugin for apispec which helps reusing the documentation on a muti-endpoint function/method.',
+      author='Brunoais',
+      author_email='brunoaiss@gmail.com',
+      url='https://github.com/brunoais/apispec-clear-unusable',
+      install_requires=['apispec'],
+      py_modules=['']
+      )
